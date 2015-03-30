71530d329769a9dc3988f5deaaeac6b0e7c3b2ad
Daniel <daniel.herold@tuwien.ac.at>
2015-03-30 16:25:43 +0200

    main.cpp mit diff

--- main.cpp
+++ main.cpp
@@ -15,11 +15,14 @@ int main( int argc, char *argv[] ) {
     LoadMatrix(matfile, tmat);
 
     MySparseMatrix mymat(tmat);
+    
 
     AutoVector vecx = mymat.CreateVector();
     vecx = 2.0;
     AutoVector vecy = mymat.CreateVector();
     vecy = 3.0;
+    AutoVector vecy1 = mymat.CreateVector();
+    vecy1 = 3.0;
     AutoVector vecy2 = mymat.CreateVector();
     vecy2 = 3.0;
     AutoVector vecy3 = mymat.CreateVector();
@@ -32,13 +35,26 @@ int main( int argc, char *argv[] ) {
     vecy5 = 3.0;
 
     mymat.MultAdd1(1.0, vecx, vecy);
+    mymat.TranMultAdd1(1.0, vecx, vecy1);
+
     mymat.MultAdd2(1.0, vecx, vecy2);
     mymat.MultAdd3(1.0, vecx, vecy3);
-    mymat.TranMultAdd1(1.0, vecx, vecy4);
     mymat.TranMultAdd2(1.0, vecx, vecy5);
     mymat.TranMultAdd3(1.0, vecx, vecy6);
 
-    diff1 = vecy - vecy2;
-    cout << "LNORM2(vecy - vecy2)" << L2Norm(diff1) << endl;
+    
+    AutoVector diff2= mymat.CreateVector();
+    AutoVector diff3= mymat.CreateVector();
+    AutoVector diff5= mymat.CreateVector();
+    AutoVector diff6= mymat.CreateVector();
+    diff2 = vecy - vecy2;
+    diff3 = vecy - vecy3;
+    diff5 = vecy1 - vecy5;
+    diff6 = vecy1 - vecy6;
+
+    cout << "LNORM2(vecy - vecy2) -sequential  " << L2Norm(diff2) << endl;
+    cout << "LNORM2(vecy - vecy3) -parallel  " << L2Norm(diff3) << endl;
+    cout << "LNORM2(vecy1 - vecy5) -Trans parallel for static  " << L2Norm(diff5) << endl;
+    cout << "LNORM2(vecy1 - vecy6) -Trans seperation  " << L2Norm(diff6) << endl;
 }
 
